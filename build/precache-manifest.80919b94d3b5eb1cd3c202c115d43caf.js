0fe2b06b075f529373aa687adbcd41e394ddb19c
Kirill-Zhernakov <50823200+Kirill-Zhernakov@users.noreply.github.com>
2021-08-12 12:47:23 +0300

    Add files via upload

---
+++ build/precache-manifest.80919b94d3b5eb1cd3c202c115d43caf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5094aaf385f02136ffa469f8296b6c70",
+    "url": "/index.html"
+  },
+  {
+    "revision": "77b92269b2c855499e1a",
+    "url": "/static/css/main.a9089c74.chunk.css"
+  },
+  {
+    "revision": "eca6562c3e8f4e50dcb6",
+    "url": "/static/js/2.51d1ca9a.chunk.js"
+  },
+  {
+    "revision": "77b92269b2c855499e1a",
+    "url": "/static/js/main.ca95621d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
